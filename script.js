629015669581dd4bf698eb35f701346d4a13e626
Danny Nguyen <nguyenduongpa@gmail.com>
2023-12-24 09:02:03 -0800

    adding javascript

---
+++ script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const scrollLinks = document.querySelectorAll('.scroll-link');
+
+    scrollLinks.forEach(link => {
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                const topOffset = document.querySelector('.top-bar').offsetHeight;
+                const targetPosition = targetElement.offsetTop - topOffset;
+
+                window.scrollTo({
+                    top: targetPosition,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+});
