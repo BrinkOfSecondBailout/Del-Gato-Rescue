c11b0f5a3160a0567ceff0b49ef0e19c545f291c
Danny Nguyen <nguyenduongpa@gmail.com>
2023-12-27 17:05:27 -0800

    form

--- server.js
+++ server.js
@@ -3,3 +3,13 @@ const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
 
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('hello world')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
