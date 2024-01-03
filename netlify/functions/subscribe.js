e7b2938838b7e104f55172d97524781385de464a
Danny Nguyen <nguyenduongpa@gmail.com>
2024-01-03 08:51:22 -0800

    troubleshooting

--- netlify/functions/subscribe.js
+++ netlify/functions/subscribe.js
@@ -28,6 +28,7 @@ exports.handler = async function (event, context) {
             body: JSON.stringify({ message: 'success' }),
         };
     } catch (error) {
+        console.log('hi')
         console.error(error);
 
         return {
